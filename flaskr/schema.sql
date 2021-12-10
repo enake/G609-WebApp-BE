05e44a23b49abcacce128d499df3d5270bd1e840
Adrian Stefan <92085719+AdrianST19@users.noreply.github.com>
2021-12-10 20:02:24 +0200

    Update schema.sql
    Am adaugat un camp nou pentru tipul de utilizator (Admin sau User)

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -4,8 +4,10 @@ CREATE TABLE users (
     user_id INTEGER PRIMARY KEY,
 	first_name TEXT NOT NULL,
 	last_name TEXT NOT NULL,
+	user_type TEXT NOT NULL,
 	email TEXT NOT NULL UNIQUE,
 	password TEXT NOT NULL
 );
 
-INSERT INTO users (first_name, last_name, email, password) VALUES ("UserFirstName", "UserLastName", "email@email.com", "cucubau");
+INSERT INTO users (first_name, last_name, email, password) VALUES ("UserFirstName", "UserLastName","Admin", "email@email.com", "cucubau");
+INSERT INTO users (first_name, last_name, email, password) VALUES ("Adi", "Stefan","User", "adi@email.com", "pass123");
